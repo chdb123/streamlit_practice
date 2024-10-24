5bb94830fbb3e0ea10d04b34c7d935f603756008
chdb123 <156301491+chdb123@users.noreply.github.com>
2024-10-24 11:43:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('👉 Machine Learning Practice')
 
 st.info('Welcome to Practice sessions')
 
-st.subheading("Let's see the labeled data")
+st.subheader("Let's see the labeled data")
 
 # Load and display data
 with st.expander("Data is"):
