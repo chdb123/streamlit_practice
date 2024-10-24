ab831f7d5251db3dc829d94b02e9b1a5f196f707
chdb123 <156301491+chdb123@users.noreply.github.com>
2024-10-24 11:28:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ with st.expander("Data is"):
   df=pd.read_csv(".devcontainer /streamlit_practice.csv")
   df
   st.write("**X**")
-  X = df.drop('Date',axis=1)
+  X = df.drop('DATE',axis=1)
   X
   st.write("**Y**")
-  Y = df.Date
+  Y = df.DATE
   Y
 
 with st.expander("Data Visualization:"):
